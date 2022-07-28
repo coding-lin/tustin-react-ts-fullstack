cb51e9f1d2de28aa97b331d0cdeca928dfef9d13
hjl <2764881569@qq.com>
2022-07-28 16:44:50 +0800

    Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { FooterWrapper } from './style'
+import classnames from 'classnames'
+
+export default function Footer() {
+  const { pathname } = useLocation()
+
+  return (
+    <FooterWrapper>
+      <Link to="/" className={classnames({active:pathname == '/'})}>
+        <i className='iconfont icon-shouye'></i>
+        <span>首页</span>
+      </Link>
+      <Link to="/food" className={classnames({active:pathname == '/food'})}>
+        <i className='iconfont icon-hanbao'></i>
+        <span>点单</span>
+      </Link>
+      <Link to="/order" className={classnames({active:pathname == '/order'})}>
+        <i className='iconfont icon-dingdan'></i>
+        <span>订单</span>
+      </Link>
+      <Link to="/mine" className={classnames({active:pathname == '/mine'})}>
+        <i className='iconfont icon-wode'></i>
+        <span>我的</span>
+      </Link>
+    </FooterWrapper>
+  )
+}
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const FooterWrapper = styled.div`
+  width: 100%;
+  height: 60px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 999;
+  background-color: #fff;
+  border-top: 1px solid rgba(50, 50, 50, 0.1);
+  display: flex;
+  a {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    color: #000;
+    &.active {
+      color: yellow;
+    }
+    i {
+      font-size: 32px;
+    }
+    span {
+      font-size: 14px;
+      color: #000;
+    }
+  }
+`
