e4661156b0c42e5c2eb69dbe37e7486a8124d406
hjl <2764881569@qq.com>
2022-07-30 13:09:57 +0800

    routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
-import React, { lazy, Suspense } from "react"
+import { lazy } from "react"
 import { Routes, Route } from 'react-router-dom'
 import Home from '@/pages/Home'  // 首页不能延迟加载
-import Food from '@/pages/Food'  // 点餐页面
-import Order from '@/pages/Order'  // 订单页面
-import Mine from '@/pages/Mine'  // 我的页面
+const Food = lazy(() => import('@/pages/Food'))  // 点餐页面
+const Order = lazy(() => import('@/pages/Order'))  // 订单页面
+const Mine = lazy(() => import('@/pages/Mine'))  // 我的页面
 
 export default () => {
   return (
