e68f8fe578575c83e25ee56c89ac54440f13e519
hjl <2764881569@qq.com>
2022-07-30 13:17:20 +0800

    banners

---
+++ tustin-api/data/banners.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    imageUrl: 'https://img1.baidu.com/it/u=3010275295,3970652084&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=324'
+  },
+  {
+    id: 2,
+    imageUrl: 'https://img2.baidu.com/it/u=3891836244,255108262&fm=253&fmt=auto&app=138&f=JPEG?w=755&h=500'
+  },
+  {
+    id: 3,
+    imageUrl: 'https://img0.baidu.com/it/u=2987151024,466181424&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=375'
+  },
+  {
+    id: 4,
+    imageUrl: 'https://img2.baidu.com/it/u=3729069389,3383299578&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=281'
+  }
+]
