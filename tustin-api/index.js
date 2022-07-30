0d936f3c124a73b1f9109e8700f98a76e5e3594d
hjl <2764881569@qq.com>
2022-07-30 13:18:33 +0800

    tustin-api

--- tustin-api/index.js
+++ tustin-api/index.js
@@ -1,9 +1,12 @@
 const koa = require('koa')
 const app = new koa()
 const cors = require('koa2-cors')
+const mainRouter = require('./routers/index')
 
 // 挂载跨域中间件
 app.use(cors())
+// 挂载路由中间件
+app.use(mainRouter)
 
 app.listen(3300, () => {
   console.log('Your app is running')
