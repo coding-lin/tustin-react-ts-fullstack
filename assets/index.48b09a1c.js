5855e7de31aacff5872b294f63eb3f1b2c75aa72
hjl <2764881569@qq.com>
2022-07-31 22:28:16 +0800

    Updates

---
+++ assets/index.48b09a1c.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.a47551f8.js";const d=()=>r("div",{children:"Order"});export{d as default};
---
+++ assets/index.f255366d.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index.a47551f8.js";const d=()=>o("div",{children:"Food"});export{d as default};
