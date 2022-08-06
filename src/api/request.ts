47c482936f48212480a99aa371938543c3728e9b
hjl <2764881569@qq.com>
2022-08-06 22:41:17 +0800

    增加getBannersList2Request

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,5 +1,8 @@
 import { axiosInstance } from "./config"
-import { URL_BANNERS } from './url'
+import { URL_BANNERS, URL_BANNERS2 } from './url'
 
 export const getBannersListRequest = () => 
   axiosInstance.get(URL_BANNERS)  // 首页轮播图
+
+  export const getBannersList2Request = () => 
+  axiosInstance.get(URL_BANNERS2)  // 点单页轮播图
--- src/api/url.ts
+++ src/api/url.ts
@@ -1,5 +1,7 @@
 const URL_BANNERS = '/banners'
+const URL_BANNERS2 = '/banners2'
 
 export {
-  URL_BANNERS
+  URL_BANNERS,
+  URL_BANNERS2
 }
