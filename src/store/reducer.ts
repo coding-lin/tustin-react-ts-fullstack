be6e9f9c45ad348f72ec22ca2c180fb6022afff5
hjl <2764881569@qq.com>
2022-08-06 22:42:55 +0800

    store

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import { reducer as bannerReducer } from '@/pages/Home/store/index'
+import { reducer as banner2Reducer } from '@/pages/Food/store/index'
 
 export default combineReducers({
-  home: bannerReducer
+  home: bannerReducer,
+  food: banner2Reducer
 })
