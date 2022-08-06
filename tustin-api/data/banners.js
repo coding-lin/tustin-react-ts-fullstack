9cc35fc966194a8913a65db11f5a6f949b68e3e7
hjl <2764881569@qq.com>
2022-08-06 21:42:09 +0800

    tustin-api

--- tustin-api/data/banners.js
+++ tustin-api/data/banners.js
@@ -1,18 +1,14 @@
 module.exports = [
   {
     id: 1,
-    imageUrl: 'https://img1.baidu.com/it/u=3010275295,3970652084&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=324'
+    imageUrl: 'https://img-blog.csdnimg.cn/34bb253060e0480eb605993508d3be06.png'
   },
   {
     id: 2,
-    imageUrl: 'https://img2.baidu.com/it/u=3891836244,255108262&fm=253&fmt=auto&app=138&f=JPEG?w=755&h=500'
+    imageUrl: 'https://img-blog.csdnimg.cn/2dc3e1dd3fc24bcd9c39c46ddb98574d.png'
   },
   {
     id: 3,
-    imageUrl: 'https://img0.baidu.com/it/u=2987151024,466181424&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=375'
-  },
-  {
-    id: 4,
-    imageUrl: 'https://img2.baidu.com/it/u=3729069389,3383299578&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=281'
+    imageUrl: 'https://img-blog.csdnimg.cn/fdca7395e0414bb5ae45148cd60305ac.png'
   }
 ]
