8a4258c095f35ea9c85b3dd8ebb2a3566c7f13f1
hjl <2764881569@qq.com>
2022-08-07 17:28:34 +0800

    api

--- src/api/request.ts
+++ src/api/request.ts
@@ -2,11 +2,15 @@ import { axiosInstance } from "./config"
 import { 
   URL_BANNERS, 
   URL_BANNERS2,
-  URL_DETAIL
+  URL_DETAIL,
+  URL_COUPONS
 } from './url'
 
 export const getBannersListRequest = () => 
   axiosInstance.get(URL_BANNERS)  // 首页轮播图
+
+export const getCouponsRequest = () => 
+  axiosInstance.get(URL_COUPONS)  // 积分商城抵用券
 
 export const getBannersList2Request = () => 
   axiosInstance.get(URL_BANNERS2)  // 点单页轮播图
--- src/api/url.ts
+++ src/api/url.ts
@@ -1,9 +1,11 @@
 const URL_BANNERS = '/banners'
 const URL_BANNERS2 = '/banners2'
 const URL_DETAIL = '/detail'
+const URL_COUPONS = '/coupons'
 
 export {
   URL_BANNERS,
   URL_BANNERS2,
-  URL_DETAIL
+  URL_DETAIL,
+  URL_COUPONS
 }
