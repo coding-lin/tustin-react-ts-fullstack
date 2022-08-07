42c5dd83d6ced9351b4ab9b8a2d1607d5ecceac2
hjl <2764881569@qq.com>
2022-08-07 12:09:32 +0800

    api

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,8 +1,15 @@
 import { axiosInstance } from "./config"
-import { URL_BANNERS, URL_BANNERS2 } from './url'
+import { 
+  URL_BANNERS, 
+  URL_BANNERS2,
+  URL_DETAIL
+} from './url'
 
 export const getBannersListRequest = () => 
   axiosInstance.get(URL_BANNERS)  // 首页轮播图
 
-  export const getBannersList2Request = () => 
+export const getBannersList2Request = () => 
   axiosInstance.get(URL_BANNERS2)  // 点单页轮播图
+
+export const getDetailRequest = () => 
+  axiosInstance.get(URL_DETAIL)  // 点单页详情
--- src/api/url.ts
+++ src/api/url.ts
@@ -1,7 +1,9 @@
 const URL_BANNERS = '/banners'
 const URL_BANNERS2 = '/banners2'
+const URL_DETAIL = '/detail'
 
 export {
   URL_BANNERS,
-  URL_BANNERS2
+  URL_BANNERS2,
+  URL_DETAIL
 }
