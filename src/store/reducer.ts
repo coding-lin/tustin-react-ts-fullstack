2ab6612ed7a7106e673f92faf948af0a6bc2a896
hjl <2764881569@qq.com>
2022-08-07 17:27:28 +0800

    store

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import { reducer as bannerReducer } from '@/pages/Home/store/index'
+import { reducer as couponsReducer } from '@/pages/PointsMall/store/index'
 import { reducer as banner2Reducer } from '@/pages/Food/store/index'
 
 export default combineReducers({
   home: bannerReducer,
+  pointsMall: couponsReducer,
   food: banner2Reducer
 })
