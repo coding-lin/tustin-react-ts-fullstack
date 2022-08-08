fd162327cadcbbfcc1a64d23e8560b059c772f57
hjl <2764881569@qq.com>
2022-08-09 00:09:27 +0800

    Order

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Empty from '@/assets/images/empty.png'
 import { OrderWrapper, TabWrapper, Container } from './style'
 
 const Order = () => {
@@ -8,27 +9,41 @@ const Order = () => {
     setTab(target)
   }
 
-  const EmptyWrapper = () => {
-    return (
-      <>
-        
-      </>
-    )
-  }
-
   const CurrOrder = () => {
     return (
-      <>
-        当前订单
-      </>
+      <div className='current-order'>
+        <img src={Empty} />
+        <span>您今天还未点单</span>
+      </div>
     )
   }
 
   const HisOrder = () => {
     return (
-      <>
-        历史订单
-      </>
+      <div className='history-order'>
+        <div className="header">
+          <div className='address'>
+            <h2>南昌市东华理工大学店</h2>
+          </div>
+          <div className="finished">
+            <span>已完成</span>
+            <i className='iconfont icon-xiangyoujiantou'></i>
+          </div>
+        </div>
+        <div className="time">
+          <span>2022-07-12 12:35:26</span>
+        </div>
+        <div className="info">
+          <div className='image'>
+            <img src="https://img-blog.csdnimg.cn/b9bc32f3497f4b7ca4eda0afd2128095.png" width="100%" height="100%" />
+          </div>
+          <div className="price">
+            <span>¥19</span>
+            <span>共2件</span>
+          </div>
+        </div>
+        <button>再来一单</button>
+      </div>
     )
   }
 
@@ -37,13 +52,13 @@ const Order = () => {
       <TabWrapper>
         <ul>
           <li 
-            className={tab == '当前订单' ? 'active' : ''}
+            className={tab === '当前订单' ? 'active' : ''}
             onClick={() => changeTab('当前订单')}
           >
             <span>当前订单</span>
           </li>
           <li 
-            className={tab == '历史订单' ? 'active' : ''}
+            className={tab === '历史订单' ? 'active' : ''}
             onClick={() => changeTab('历史订单')}
           >
             <span>历史订单</span>
@@ -51,7 +66,7 @@ const Order = () => {
         </ul>
       </TabWrapper>
       <Container>
-        { tab == '当前订单' ? CurrOrder() : HisOrder() }
+        { tab === '当前订单' ? CurrOrder() : HisOrder() }
       </Container>
     </OrderWrapper>
   )
--- src/pages/Order/style.ts
+++ src/pages/Order/style.ts
@@ -45,6 +45,101 @@ export const TabWrapper = styled.div`
 `
 
 export const Container = styled.div`
-  text-align: center;
-  margin-top: 100px;
+  width: 100%;
+  .current-order {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: ${px2rem(169)};
+    margin-bottom: ${px2rem(262)};
+    img {
+      width: ${px2rem(200)};
+      height: ${px2rem(220)};
+    }
+    span {
+      font-size: ${px2rem(16)}; 
+      color: #999;
+    }
+  }
+  .history-order {
+    width: 94%;
+    height: ${px2rem(182)};
+    margin-top: ${px2rem(75)};
+    margin-bottom: ${px2rem(410)};
+    margin-left: auto;
+    margin-right: auto;
+    background: #fff;
+    border-radius: ${px2rem(10)};
+    .header {
+      display: flex;
+      justify-content: space-between;
+      .address {
+        margin-top: ${px2rem(10)};
+        margin-left: ${px2rem(10)};
+        padding: ${px2rem(5)};
+        h2 {
+          font-size: ${px2rem(16)};
+          font-weight: 600;
+        }
+      }
+      .finished {
+        margin-top: ${px2rem(8)};
+        margin-right: ${px2rem(15)};
+        padding: ${px2rem(2)};
+        color: #999;
+        font-weight: 600;
+        span {
+          font-size: ${px2rem(13)};
+        }
+        i {
+          margin-left: ${px2rem(2)};
+          font-size: ${px2rem(12)};
+        }
+      }
+    }
+    .time {
+      margin-left: ${px2rem(16)};
+      span {
+        color: #999;
+        font-size: ${px2rem(14)};
+      }
+    }
+    .info {
+      display: flex;
+      justify-content: space-between;
+      .image {
+        margin-top: ${px2rem(10)};
+        margin-left: ${px2rem(16)};
+        width: ${px2rem(142)};
+        height: ${px2rem(70)};
+      }
+      .price {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-right: ${px2rem(20)};
+        >span:first-child() {
+          font-size: ${px2rem(16)};
+          font-weight: 600;
+        }
+        >span:nth-child(2) {
+          margin-top: ${px2rem(10)};
+          color: #999;
+          font-size: ${px2rem(14)};
+        }
+      }
+    }
+    button {
+      margin-right: ${px2rem(20)};
+      border: 0;
+      border-radius: ${px2rem(6)};
+      float: right;
+      background: red;
+      color: #fff;
+      width: ${px2rem(80)};
+      height: ${px2rem(34)};
+      font-size: ${px2rem(14)};
+    }
+  }
 `
