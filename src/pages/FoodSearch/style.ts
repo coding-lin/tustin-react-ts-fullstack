83ae4b305861166cbc2191812fec6d1459926fc6
hjl <2764881569@qq.com>
2022-08-11 21:32:22 +0800

    FoodSearch

--- src/pages/FoodSearch/style.ts
+++ src/pages/FoodSearch/style.ts
@@ -2,18 +2,24 @@ import styled from "styled-components";
 import { px2rem } from "@/assets/global-style";
 
 export const HeaderWrapper = styled.div`
-  position: fixed;
   width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
   height: ${px2rem(40)};
   background: #fff;
-  font-size: ${px2rem(18)};
-  font-weight: 600;
+  z-index: 999;
   line-height: ${px2rem(40)};
   i {
+    font-size: ${px2rem(18)};
+    font-weight: 600;
     padding: ${px2rem(4)};
     margin-left: ${px2rem(8)};
   }
   span {
+    font-size: ${px2rem(18)};
+    font-weight: 600;
     margin-left: ${px2rem(5)};
   }
 `
