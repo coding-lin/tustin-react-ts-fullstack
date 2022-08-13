feec09d1444c3e3924c7b190204805a462c11338
hjl <2764881569@qq.com>
2022-08-13 22:10:41 +0800

    tustin-api

--- tustin-api/data/detail.js
+++ tustin-api/data/detail.js
@@ -8,28 +8,32 @@ module.exports = [
         name: "就是黑凤梨中国汉堡",
         img: "	https://img-blog.csdnimg.cn/97b9de5ed3c84ca897a643df29395df8.png",
         info: "★堡胚含墨鱼汁成分，海鲜过敏人群请谨慎使用 ★单层牛肉堡",
-        price: 17
+        price: 17,
+        number: 0
       },
       {
         id: 2,
         name: "加倍黑凤梨中国汉堡",
         img: "https://img-blog.csdnimg.cn/b7a8a2e538d64731b7f01de0774fa9f5.png",
         info: "★堡胚含墨鱼汁成分，海鲜过敏人群请谨慎使用 ★双层牛肉堡",
-        price: 23
+        price: 23,
+        number: 0
       },
       {
         id: 3,
         name: "黑凤梨单人餐",
         img: "https://img-blog.csdnimg.cn/afdabceb72fd4a1d99bc0074e8b034fd.png",
         info: "就是黑凤梨中国汉堡 (单层牛肉) +肯定好鸡脖+青梅见喜汽水",
-        price: 25
+        price: 25,
+        number: 0
       },
       {
         id: 4,
         name: "黑凤梨双人餐",
         img: "https://img-blog.csdnimg.cn/ccab0a74ddce4de6bd65fc365c4a791e.png",
         info: "就是黑凤梨中国汉堡 (单层牛肉) +香辣鸡腿中国汉堡+粗薯",
-        price: 39
+        price: 39,
+        number: 0
       }
     ]
   },
@@ -42,21 +46,24 @@ module.exports = [
         name: "储值赠单人套餐",
         img: "https://img-blog.csdnimg.cn/a4d2274d69c647da980c19d1c18a6dd1.png",
         info: "香辣鸡腿中国汉堡+香辣鸡翅+冰柠可乐",
-        price: 27
+        price: 27,
+        number: 0
       },
       {
         id: 2,
         name: "储值赠双人套餐",
         img: "https://img-blog.csdnimg.cn/581ad4e11d644896baa8b9c0fc31d19b.png",
         info: "香辣鸡腿中国汉堡+多汁牛肉中国汉堡+上校鸡块+盐酥鸡米花+冰柠可乐*2",
-        price: 57
+        price: 57,
+        number: 0
       },
       {
         id: 3,
         name: "储值赠家庭套餐",
         img: "https://img-blog.csdnimg.cn/b8d73686f6dd425c8f966c7150e0a15a.png",
         info: "香辣鸡腿中国汉堡*2+蜜汁手扒鸡+盐酥鸡米花+粗薯+冰柠可乐*3",
-        price: 86
+        price: 86,
+        number: 0
       }
     ]
   },
@@ -69,35 +76,40 @@ module.exports = [
         name: "爆爆新拍档单人餐",
         img: "https://img-blog.csdnimg.cn/8facabf2157942639701d3f96c553c46.png",
         info: "香辣鸡腿中国汉堡 + 脆脆肉多多 + 粗薯 + 冰柠可乐",
-        price: 25
+        price: 25,
+        number: 0
       },
       {
         id: 2,
         name: "爆爆新拍档双人餐",
         img: "https://img-blog.csdnimg.cn/b8ccc00467714c47a2eb265113db2189.png",
         info: "香辣鸡腿中国汉堡 + 培根煎蛋中国汉堡 + 脆脆肉多多 + 香辣鸡翅 + 冰柠可乐*2",
-        price: 39
+        price: 39,
+        number: 0
       },
       {
         id: 3,
         name: "爆爆新拍档单人餐",
         img: "https://img-blog.csdnimg.cn/bfd1cb95d8d04fa7a32b0e9ebaf1fbb2.png",
         info: "香辣鸡腿中国汉堡 + 香辣鸡翅 + 超赞海盐石榴汁",
-        price: 21.9
+        price: 21.9,
+        number: 0
       },
       {
         id: 4,
         name: "爆爆新拍档下午茶套餐",
         img: "https://img-blog.csdnimg.cn/974895dc7c8f4c4dbaf9c57b7d4b6534.png",
         info: "啃定好鸡脖 + 香辣鸡翅 + 超赞海盐石榴汁",
-        price: 16.9
+        price: 16.9,
+        number: 0
       },
       {
         id: 5,
         name: "「青青见喜」单人堡餐",
         img: "https://img-blog.csdnimg.cn/8dfb5fb5a5414c91bcb7095b324056f8.png",
         info: "香辣鸡腿中国汉堡 + 粗薯 + 青青见喜气泡饮",
-        price: 19.9
+        price: 19.9,
+        number: 0
       }
     ]
   },
@@ -110,42 +122,48 @@ module.exports = [
         name: "单人套餐一",
         img: "https://img-blog.csdnimg.cn/2d9ac44f244243bcbc0d2c2de8a77eac.png",
         info: "香辣鸡腿中国汉堡 + 香辣鸡翅 + 冰柠可乐",
-        price: 19.9
+        price: 19.9,
+        number: 0
       },
       {
         id: 2,
         name: "单人套餐二",
         img: "https://img-blog.csdnimg.cn/76d13d72766d4e31bf0cc6d729d40544.png",
         info: "北京烤鸭中国汉堡 + 上校鸡块 + 冰柠可乐",
-        price: 25
+        price: 25,
+        number: 0
       },
       {
         id: 3,
         name: "双人套餐一",
         img: "https://img-blog.csdnimg.cn/12f7e98c8a40493e8d93e0023e37ca28.png",
         info: "香辣鸡腿中国汉堡*2 + 粗薯 + 冰柠可乐*2",
-        price: 36
+        price: 36,
+        number: 0
       },
       {
         id: 4,
         name: "双人套餐二",
         img: "https://img-blog.csdnimg.cn/621d981aa9394d3eb7fcb15cd8404d4f.png",
         info: "香辣鸡腿中国汉堡 + 多汁牛肉中国汉堡 + 上校鸡块 + 冰柠可乐*2",
-        price: 39
+        price: 39,
+        number: 0
       },
       {
         id: 5,
         name: "乐享下午茶",
         img: "https://img-blog.csdnimg.cn/75b506833b5d4548b9dbb33d82e9644f.png",
         info: "香辣鸡翅 + 粗薯 + 柠檬红茶",
-        price: 16
+        price: 16,
+        number: 0
       },
       {
         id: 6,
         name: "缤纷下午茶",
         img: "https://img-blog.csdnimg.cn/f76912fc1d034550949d87a6e899edf5.png",
         info: "上校鸡块 + 粗薯 + 尖叫翅尖",
-        price: 21
+        price: 21,
+        number: 0
       }
     ]
   },
@@ -158,35 +176,40 @@ module.exports = [
         name: "爆爆新拍档单人餐",
         img: "https://img-blog.csdnimg.cn/8facabf2157942639701d3f96c553c46.png",
         info: "香辣鸡腿中国汉堡 + 脆脆肉多多 + 粗薯 + 冰柠可乐",
-        price: 25
+        price: 25,
+        number: 0
       },
       {
         id: 2,
         name: "爆爆新拍档双人餐",
         img: "https://img-blog.csdnimg.cn/b8ccc00467714c47a2eb265113db2189.png",
         info: "香辣鸡腿中国汉堡 + 培根煎蛋中国汉堡 + 脆脆肉多多 + 香辣鸡翅 + 冰柠可乐*2",
-        price: 39
+        price: 39,
+        number: 0
       },
       {
         id: 3,
         name: "爆爆新拍档单人餐",
         img: "https://img-blog.csdnimg.cn/bfd1cb95d8d04fa7a32b0e9ebaf1fbb2.png",
         info: "香辣鸡腿中国汉堡 + 香辣鸡翅 + 超赞海盐石榴汁",
-        price: 21.9
+        price: 21.9,
+        number: 0
       },
       {
         id: 4,
         name: "爆爆新拍档下午茶套餐",
         img: "https://img-blog.csdnimg.cn/974895dc7c8f4c4dbaf9c57b7d4b6534.png",
         info: "啃定好鸡脖 + 香辣鸡翅 + 超赞海盐石榴汁",
-        price: 16.9
+        price: 16.9,
+        number: 0
       },
       {
         id: 5,
         name: "「青青见喜」单人堡餐",
         img: "https://img-blog.csdnimg.cn/8dfb5fb5a5414c91bcb7095b324056f8.png",
         info: "香辣鸡腿中国汉堡 + 粗薯 + 青青见喜气泡饮",
-        price: 19.9
+        price: 19.9,
+        number: 0
       }
     ]
   },
@@ -199,42 +222,48 @@ module.exports = [
         name: "单人套餐一",
         img: "https://img-blog.csdnimg.cn/2d9ac44f244243bcbc0d2c2de8a77eac.png",
         info: "香辣鸡腿中国汉堡 + 香辣鸡翅 + 冰柠可乐",
-        price: 19.9
+        price: 19.9,
+        number: 0
       },
       {
         id: 2,
         name: "单人套餐二",
         img: "https://img-blog.csdnimg.cn/76d13d72766d4e31bf0cc6d729d40544.png",
         info: "北京烤鸭中国汉堡 + 上校鸡块 + 冰柠可乐",
-        price: 25
+        price: 25,
+        number: 0
       },
       {
         id: 3,
         name: "双人套餐一",
         img: "https://img-blog.csdnimg.cn/12f7e98c8a40493e8d93e0023e37ca28.png",
         info: "香辣鸡腿中国汉堡*2 + 粗薯 + 冰柠可乐*2",
-        price: 36
+        price: 36,
+        number: 0
       },
       {
         id: 4,
         name: "双人套餐二",
         img: "https://img-blog.csdnimg.cn/621d981aa9394d3eb7fcb15cd8404d4f.png",
         info: "香辣鸡腿中国汉堡 + 多汁牛肉中国汉堡 + 上校鸡块 + 冰柠可乐*2",
-        price: 39
+        price: 39,
+        number: 0
       },
       {
         id: 5,
         name: "乐享下午茶",
         img: "https://img-blog.csdnimg.cn/75b506833b5d4548b9dbb33d82e9644f.png",
         info: "香辣鸡翅 + 粗薯 + 柠檬红茶",
-        price: 16
+        price: 16,
+        number: 0
       },
       {
         id: 6,
         name: "缤纷下午茶",
         img: "https://img-blog.csdnimg.cn/f76912fc1d034550949d87a6e899edf5.png",
         info: "上校鸡块 + 粗薯 + 尖叫翅尖",
-        price: 21
+        price: 21,
+        number: 0
       }
     ]
   },
@@ -247,35 +276,40 @@ module.exports = [
         name: "爆爆新拍档单人餐",
         img: "https://img-blog.csdnimg.cn/8facabf2157942639701d3f96c553c46.png",
         info: "香辣鸡腿中国汉堡 + 脆脆肉多多 + 粗薯 + 冰柠可乐",
-        price: 25
+        price: 25,
+        number: 0
       },
       {
         id: 2,
         name: "爆爆新拍档双人餐",
         img: "https://img-blog.csdnimg.cn/b8ccc00467714c47a2eb265113db2189.png",
         info: "香辣鸡腿中国汉堡 + 培根煎蛋中国汉堡 + 脆脆肉多多 + 香辣鸡翅 + 冰柠可乐*2",
-        price: 39
+        price: 39,
+        number: 0
       },
       {
         id: 3,
         name: "爆爆新拍档单人餐",
         img: "https://img-blog.csdnimg.cn/bfd1cb95d8d04fa7a32b0e9ebaf1fbb2.png",
         info: "香辣鸡腿中国汉堡 + 香辣鸡翅 + 超赞海盐石榴汁",
-        price: 21.9
+        price: 21.9,
+        number: 0
       },
       {
         id: 4,
         name: "爆爆新拍档下午茶套餐",
         img: "https://img-blog.csdnimg.cn/974895dc7c8f4c4dbaf9c57b7d4b6534.png",
         info: "啃定好鸡脖 + 香辣鸡翅 + 超赞海盐石榴汁",
-        price: 16.9
+        price: 16.9,
+        number: 0
       },
       {
         id: 5,
         name: "「青青见喜」单人堡餐",
         img: "https://img-blog.csdnimg.cn/8dfb5fb5a5414c91bcb7095b324056f8.png",
         info: "香辣鸡腿中国汉堡 + 粗薯 + 青青见喜气泡饮",
-        price: 19.9
+        price: 19.9,
+        number: 0
       }
     ]
   },
@@ -288,42 +322,48 @@ module.exports = [
         name: "单人套餐一",
         img: "https://img-blog.csdnimg.cn/2d9ac44f244243bcbc0d2c2de8a77eac.png",
         info: "香辣鸡腿中国汉堡 + 香辣鸡翅 + 冰柠可乐",
-        price: 19.9
+        price: 19.9,
+        number: 0
       },
       {
         id: 2,
         name: "单人套餐二",
         img: "https://img-blog.csdnimg.cn/76d13d72766d4e31bf0cc6d729d40544.png",
         info: "北京烤鸭中国汉堡 + 上校鸡块 + 冰柠可乐",
-        price: 25
+        price: 25,
+        number: 0
       },
       {
         id: 3,
         name: "双人套餐一",
         img: "https://img-blog.csdnimg.cn/12f7e98c8a40493e8d93e0023e37ca28.png",
         info: "香辣鸡腿中国汉堡*2 + 粗薯 + 冰柠可乐*2",
-        price: 36
+        price: 36,
+        number: 0
       },
       {
         id: 4,
         name: "双人套餐二",
         img: "https://img-blog.csdnimg.cn/621d981aa9394d3eb7fcb15cd8404d4f.png",
         info: "香辣鸡腿中国汉堡 + 多汁牛肉中国汉堡 + 上校鸡块 + 冰柠可乐*2",
-        price: 39
+        price: 39,
+        number: 0
       },
       {
         id: 5,
         name: "乐享下午茶",
         img: "https://img-blog.csdnimg.cn/75b506833b5d4548b9dbb33d82e9644f.png",
         info: "香辣鸡翅 + 粗薯 + 柠檬红茶",
-        price: 16
+        price: 16,
+        number: 0
       },
       {
         id: 6,
         name: "缤纷下午茶",
         img: "https://img-blog.csdnimg.cn/f76912fc1d034550949d87a6e899edf5.png",
         info: "上校鸡块 + 粗薯 + 尖叫翅尖",
-        price: 21
+        price: 21,
+        number: 0
       }
     ]
   },
@@ -336,35 +376,40 @@ module.exports = [
         name: "爆爆新拍档单人餐",
         img: "https://img-blog.csdnimg.cn/8facabf2157942639701d3f96c553c46.png",
         info: "香辣鸡腿中国汉堡 + 脆脆肉多多 + 粗薯 + 冰柠可乐",
-        price: 25
+        price: 25,
+        number: 0
       },
       {
         id: 2,
         name: "爆爆新拍档双人餐",
         img: "https://img-blog.csdnimg.cn/b8ccc00467714c47a2eb265113db2189.png",
         info: "香辣鸡腿中国汉堡 + 培根煎蛋中国汉堡 + 脆脆肉多多 + 香辣鸡翅 + 冰柠可乐*2",
-        price: 39
+        price: 39,
+        number: 0
       },
       {
         id: 3,
         name: "爆爆新拍档单人餐",
         img: "https://img-blog.csdnimg.cn/bfd1cb95d8d04fa7a32b0e9ebaf1fbb2.png",
         info: "香辣鸡腿中国汉堡 + 香辣鸡翅 + 超赞海盐石榴汁",
-        price: 21.9
+        price: 21.9,
+        number: 0
       },
       {
         id: 4,
         name: "爆爆新拍档下午茶套餐",
         img: "https://img-blog.csdnimg.cn/974895dc7c8f4c4dbaf9c57b7d4b6534.png",
         info: "啃定好鸡脖 + 香辣鸡翅 + 超赞海盐石榴汁",
-        price: 16.9
+        price: 16.9,
+        number: 0
       },
       {
         id: 5,
         name: "「青青见喜」单人堡餐",
         img: "https://img-blog.csdnimg.cn/8dfb5fb5a5414c91bcb7095b324056f8.png",
         info: "香辣鸡腿中国汉堡 + 粗薯 + 青青见喜气泡饮",
-        price: 19.9
+        price: 19.9,
+        number: 0
       }
     ]
   },
@@ -377,42 +422,48 @@ module.exports = [
         name: "单人套餐一",
         img: "https://img-blog.csdnimg.cn/2d9ac44f244243bcbc0d2c2de8a77eac.png",
         info: "香辣鸡腿中国汉堡 + 香辣鸡翅 + 冰柠可乐",
-        price: 19.9
+        price: 19.9,
+        number: 0
       },
       {
         id: 2,
         name: "单人套餐二",
         img: "https://img-blog.csdnimg.cn/76d13d72766d4e31bf0cc6d729d40544.png",
         info: "北京烤鸭中国汉堡 + 上校鸡块 + 冰柠可乐",
-        price: 25
+        price: 25,
+        number: 0
       },
       {
         id: 3,
         name: "双人套餐一",
         img: "https://img-blog.csdnimg.cn/12f7e98c8a40493e8d93e0023e37ca28.png",
         info: "香辣鸡腿中国汉堡*2 + 粗薯 + 冰柠可乐*2",
-        price: 36
+        price: 36,
+        number: 0
       },
       {
         id: 4,
         name: "双人套餐二",
         img: "https://img-blog.csdnimg.cn/621d981aa9394d3eb7fcb15cd8404d4f.png",
         info: "香辣鸡腿中国汉堡 + 多汁牛肉中国汉堡 + 上校鸡块 + 冰柠可乐*2",
-        price: 39
+        price: 39,
+        number: 0
       },
       {
         id: 5,
         name: "乐享下午茶",
         img: "https://img-blog.csdnimg.cn/75b506833b5d4548b9dbb33d82e9644f.png",
         info: "香辣鸡翅 + 粗薯 + 柠檬红茶",
-        price: 16
+        price: 16,
+        number: 0
       },
       {
         id: 6,
         name: "缤纷下午茶",
         img: "https://img-blog.csdnimg.cn/f76912fc1d034550949d87a6e899edf5.png",
         info: "上校鸡块 + 粗薯 + 尖叫翅尖",
-        price: 21
+        price: 21,
+        number: 0
       }
     ]
   }
